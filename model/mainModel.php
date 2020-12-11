df74a944563c4b78c64bb87bc8e5106f62bebe41
= <=>
2020-12-11 12:13:40 -0400

    parametros para heroku

--- model/mainModel.php
+++ model/mainModel.php
@@ -24,7 +24,7 @@
 
 		public static function connectDB(){
 
-/*
+
 		try {			
 
 $db = (function(){
@@ -39,7 +39,7 @@ return $db;
 		} catch (PDOException $e) {
 		    error_log("Failed to connect to database: ".$e->getMessage());
 		}				
-/**/
+/*
 
 
 		try {			
@@ -633,7 +633,7 @@ protected static function isFieldsEqualToThoseInTheDatabase($queryToGet,$fieldst
     
     //* DB connection
      
-
+/*
     $gaSql['link'] = pg_connect(
         " host=".$gaSql['server'].
         " dbname=".$gaSql['db'].
@@ -643,7 +643,7 @@ protected static function isFieldsEqualToThoseInTheDatabase($queryToGet,$fieldst
 
 
 /**/
-/*
+
  $db_url = getenv("DATABASE_URL") ?: "postgres://ilsmpwdzrresby:7879db47bd3be54c574eab3a81a1bc2db477bc890a756eccc406992832a0fd8e@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/d4hub5gh1m9jjj";
 
 $gaSql['link'] = pg_connect($db_url);
