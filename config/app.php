ab902a8abd223ba6ee6f70cf2fa1a481f5b48588
= <=>
2021-03-08 15:39:36 -0400

    parametros para el server online

---
+++ config/app.php
@@ -0,0 +1,22 @@
+<?php 
+
+const IF_LOCAL_SERVER =false;
+
+
+const SERVERURL="https://gestion-epidemiologica-paraiso.herokuapp.com/";
+
+//const SERVERURL = "http://localhost/projects/dptoEpidemi/";
+
+const ORGANIZATION = "Clinica Popular el Paraiso: Dpto de Epidemiologia";
+
+const BASE_DIRECTORY = '/var/www/server/projects/dptoEpidemi/';
+
+date_default_timezone_set("America/Caracas");
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+ini_set('error_reporting', E_ALL);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+?>
